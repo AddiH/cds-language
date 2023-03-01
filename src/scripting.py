dd7ebb58d31d2501e71c36581b8c85896fc64f3c
“AddiH” <astrid.elmann@gmail.com>
2023-03-01 12:00:46 +0100

    notes from in class code along on scripting

--- src/scripting.py
+++ src/scripting.py
@@ -16,29 +16,32 @@
 # 
 # hello(args.name)
 
-import argparse
+import argparse # when you import, it runs everything in the files (thats how the functions gets defined)
+# so when you write functions you can add if __name__ == "__main__": (see bottom) then what is after that is 
+# only run if it is run for command line, not when it is imported as a module
 
 def input_parse():
     #initialise parser
     parser = argparse.ArgumentParser()
     # add arguments
-    parser.add_argument("--name", type=str)
-    parser.add_argument("--age", type=str)
+    parser.add_argument("--name", type = str, default = "world")
+    parser.add_argument("--age", type = int, required=True) # do not even THINK about running the code if age is not defined
     # parse the arguments from command line
     args = parser.parse_args()
-    # get the name
-    name = args.name
-    age = args.age
-    return name, age
+    # get the variables
+    return args
 
 
 def hello(name, age):
-    print("what are you doing here " + name + ". It's past your bedtime!, you are only " + age + " years old!")
+    print("what are you doing here " + name + f"!?. It's past your bedtime! You are only {age} years old!")
+    print("str() is also good when you're " + str(age) + " years old")
 
 def main():
     # run input parse to get name
-    name, age = input_parse()
+    args = input_parse()
     # pass name to hello
-    hello(name, age)
+    hello(args.name, args.age)
 
-main()
+# if this code is called from command line, run main
+if __name__ == "__main__": # which file is being read? The __name__ is name of current file, the __main__ is the file being run
+    main()
